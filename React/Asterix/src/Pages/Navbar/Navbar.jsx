441b8efca9a9bb361c7693709b0f6e3de585764c
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2024-03-22 17:22:04 +0100

    feat<navbar> : création de la navbar

---
+++ React/Asterix/src/Pages/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="container">
+                <nav className="navbar">
+                    <a href="/">Accueil</a>
+                    <a href="/attractions">Attractions</a>
+                    <a href="/missions">Missions</a>
+                    <div className="logs">
+                        <a className='inscription'href="/inscription">Connection</a>
+                    </div>
+                </nav>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
