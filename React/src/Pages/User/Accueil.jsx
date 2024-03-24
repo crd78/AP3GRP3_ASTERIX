1fc3dc0e40bb314e7ce8b7345c635f2381e2f185
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2024-03-24 15:19:15 +0100

    feat<accueil>: debut page accueil

---
+++ React/src/Pages/User/Accueil.jsx
@@ -0,0 +1,11 @@
+import "./Accueil.css"
+
+const Accueil = () => {
+    return (
+        <div className="container">
+            <div className="accueil">
+                
+            </div>
+        </div>
+    );
+}
