3adb0d482b568007719c53e4b58b74a061f6b816
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2024-03-26 23:57:14 +0100

    feat<api>:reprise api

--- Api/Routes/User/RouteUser.js
+++ Api/Routes/User/RouteUser.js
@@ -1,12 +1,32 @@
 const express = require('express');
 // Initialize express router
-let router = require('express').Router();
+const router = express.Router();
+const jwt = require('jsonwebtoken');
 // Set default API response
+const connectDB = require('../../app').connectDB;
 
-//route pour tester si mes fichier communique entre eux
-router.get('/users', (req, res) => {
-    res.send('This is the users page');
-  });
+//fonction dit middleware (permet de verifier l'état du token avant d'aller récupérer les informations pour notre utilisateur )
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+    const token = authHeader && authHeader.split(' ')[1];
+  
+    if (token == null) return res.sendStatus(401)
+  
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(401);
+      }
+      req.user = user;
+      next();
+    });
+  }
+
+  //route pour tester si mes fichier communique entre eux
+router.get('/Admin', (req, res) => {
+    res.send('This is the admin page');
+});
+
 //route pour tester la connexion a la bdd
 router.get('/', (req, res) => {
     res.json({
@@ -15,27 +35,17 @@ router.get('/', (req, res) => {
     });
 });
 
-// // routes pour se connecter
-// app.post('/login', (req, res) => {
-//     const username = req.body.username;
-//     const password = req.body.password;
-  
-//     db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, rows) => {
-//       if (err) {
-//         logger.error(err);
-//       } else {
-//         if (rows.length > 0) {
-//           user = {
-//             username: username,
-//             password: password
-//           };
-//           const accessToken = generateToken(user);
-//           res.json({ accessToken: accessToken });
-//         } else {
-//           res.send('Username ou password incorrect');
-//         }
-//       }
-//     });
-// });
+  //route pour récupérer les informations de l'utilisateur
+router.get('/user', authenticateToken, (req, res) => {
+    const connection = connectDB();
+    connection.query('SELECT * FROM user', function (err, rows, fields) {
+        if (err) throw err;
+        res.json(rows);
+    });
+});;
 
-module.exports = router;
+// Export API routes
+exports.router = router;
+
+
+
--- Api/app.js
+++ Api/app.js
@@ -7,86 +7,27 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const mysql = require('mysql2');
 let router = require('express').Router();
-const userRoutes = require('./Routes/User/RouteUser.js');
+
 const adminRoutes = require('./Routes/Admin/RouteAdmin.js');
 
-app.use('/users', userRoutes);
-app.use('/admin', adminRoutes);
-
-// connexion à la base de données
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: '',//nom de la bdd
-});
-
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
-const config = require('./app.js');
-let user = {};
-
-function generateToken(us){
-    return jwt.sign(us, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1m' });
-    }
-  
-  // Configuration de winston
-  const logger = winston.createLogger({
-    transports: [
-      new winston.transports.File({ filename: 'app.log' }),
-    ],
-});
 
 app.use(bodyParser.json());
 app.use(cors());
 
-//fonction dit middleware (permet de verifier l'état du token avant d'aller récupérer les informations pour notre utilisateur )
-function authenticateToken(req, res, next) {
-    const authHeader = req.headers['authorization'];
-    console.log(authHeader);
-    const token = authHeader && authHeader.split(' ')[1];
-  
-    if (token == null) return res.sendStatus(401)
-  
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(401);
-      }
-      req.user = user;
-      next();
+app.use('/admin', adminRoutes);
+
+//fonction qui se connecte a la db
+function connectDB() {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'tkt'
     });
+    return connection;
 }
 
-
-
-//routes pour verifier un token
-app.post('/token', (req, res) => {
-    const token = req.body.token;
-  
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-      logger.json(user);
-    });
-});
-
-// routes pour ressortir toute les attractions
-app.get('/attractions', (req, res) => {
-    db.query('SELECT * FROM attractions', (err, rows) => {
-        if (err) {
-            logger.error(err);
-        } else {
-            res.send(rows);
-        }
-    });
-});
-
+module.exports = connectDB;
 
 
 app.listen(3000, () => {
