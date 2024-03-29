66e9e783cfe686f372bebc94c916d09ae928c296
carlos rodrigues dias <dias2001.rodrigues@gmail.com>
2024-03-29 23:49:25 +0100

    feat<isadmin> correctiond de probleme

--- React/src/Pages/Navbar/Navbar.jsx
+++ React/src/Pages/Navbar/Navbar.jsx
@@ -2,30 +2,47 @@
 import './Navbar.css';
 import {Link} from 'react-router-dom';
 import {UserContext} from '../../assets/Context/UserContexte';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
+import { useState } from 'react';
 
 
 const Navbar = () => {
     const { isAdmin } = useContext(UserContext);
+    const [isOpen, setisOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 100);
 
-    console.log("coucou2", isAdmin ? isAdmin() : null, Date.now());
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            setIsMobile(window.innerWidth < 1000);
+        });
+    }, []);
 
     return (
         <nav>
             <div className="container">
                 <nav className="navbar">
-                    <a href="/">Accueil</a>
-                    <a href="/attractions">Attractions</a>
-                    <a href="/missions">Missions</a>
-                  
-                    {isAdmin && isAdmin() && (
-                        <Link to="/admin/avertissements">Admin Link</Link>
-                    )} 
-                    {isAdmin && !isAdmin() && (
-                        <Link to="/Avertissement">User Link</Link>
+                    {isMobile && (
+                        <button onClick={() => setisOpen(!isOpen)}>
+                            {isOpen ? 'Fermer' : 'Ouvrir'}
+                        </button>
                     )}
-
-                    <a href="/login">Connexion</a>
+                    {(isOpen || !isMobile) && (
+                        <div className="nav-links">
+                            <a href="/">Accueil</a>
+                            <a href="/attractions">Attractions</a>
+                            <a href="/missions">Missions</a>
+                    
+                            {isAdmin && isAdmin() && (
+                                <Link to="/admin/avertissements">Gérer Avertissement</Link>
+                            )} 
+                            {isAdmin && !isAdmin() && (
+                                <Link to="/Avertissement">Avertissement</Link>
+                            )}
+                            <div className="Connexion">
+                                <a href="/login">Connexion</a>
+                            </div>
+                        </div>
+                        )}
                 </nav>
             </div>
         </nav>
